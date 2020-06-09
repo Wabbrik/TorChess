6360897a13ea20a2cec4fa7d33b8ad039b794aca
Wabbrik <gabriel.marton@ulbsibiu.ro>
2020-06-09 13:39:56 +0300

    mouse hover interaction

--- UI/Form1.cs
+++ UI/Form1.cs
@@ -31,52 +31,31 @@ namespace TorChess
                     pictureGrid[Row, Col] = new PictureBox();
                     pictureGrid[Row, Col].Size = new Size(width, height);
                     pictureGrid[Row, Col].Location = new Point(Col * width, Row * height);
-                    if ((Col + Row) % 2 == 0) 
-                    { 
-                        pictureGrid[Row, Col].BackColor = Color.Black; 
+                    pictureGrid[Row, Col].MouseEnter += Picture_Box_MouseEnter;
+                    pictureGrid[Row, Col].MouseLeave += Picture_Box_MouseLeave;
+                    if ((Col + Row) % 2 == 0)
+                    {
+                        pictureGrid[Row, Col].BackColor = Color.Black;
                     }
                     else
                     {
                         pictureGrid[Row, Col].BackColor = Color.White;
                     }
+                    pictureGrid[Row, Col].Tag = pictureGrid[Row, Col].BackColor;
                     panel1.Controls.Add(pictureGrid[Row, Col]);
                 }
             }
 
         }
-
-        private void TorchessForm_Load_1(object sender, EventArgs e)
+        private void Picture_Box_MouseLeave(object sender, EventArgs e)
         {
-            // PopulatePictureGrid(70);
-            //Bitmap bm = new Bitmap(8 * 100, 8 * 100);
-            //Graphics g = Graphics.FromImage(bm);
-            //Color color1, color2;
-            //for (int i = 0; i < 8; i++)
-            //{
-            //    if (i % 2 == 0)
-            //    {
-            //        color1 = Color.Black;
-            //        color2 = Color.White;
-            //    }
-            //    else
-            //    {
-            //        color1 = Color.White;
-            //        color2 = Color.Black;
-            //    }
-            //    SolidBrush blackBrush = new SolidBrush(color1);
-            //    SolidBrush whiteBrush = new SolidBrush(color2);
-
-            //    for (int j = 0; j < 8; j++)
-            //    {
-            //        if (j % 2 == 0)
-            //            g.FillRectangle(blackBrush, i * 100, j * 100, 100, 100);
-            //        else
-            //            g.FillRectangle(whiteBrush, i * 100, j * 100, 100, 100);
-            //    }
-            //}
-
-            //g.DrawImage(bm, 150, 200);
-            //BackgroundImage = bm;
+            PictureBox hoveredPicture = (PictureBox)sender;
+            hoveredPicture.BackColor = (Color)hoveredPicture.Tag;
+        }
+        private void Picture_Box_MouseEnter(object sender, EventArgs e)
+        {
+            PictureBox hoveredPicture = (PictureBox)sender;
+            hoveredPicture.BackColor = Color.LightGreen;
         }
     }
 }
