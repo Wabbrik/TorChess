082821c961b4ae3e1c233d3b35d6721d4080c9aa
FlorinCiobotea <florin.ciobotea@ulbsibiu.ro>
2020-06-09 14:48:07 +0300

    Inner and Outer Pawns

--- Common/InnerPawn.cs
+++ Common/InnerPawn.cs
@@ -20,15 +20,68 @@ namespace TorChess.Common
         }
         public override bool CanMove(int SrcRow, int SrcCol, int DestRow, int DestCol, Piece[,] board)
         {
-            Piece destPiece = board[DestRow, DestCol];
-            if(destPiece == null)
+          
+            Piece destPiece = board[DestRow,DestCol];
+            if (destPiece == null)
             {
-                //move(BY COLOR)
+                if (DestRow == SrcRow)
+                {
+                    if (color == 'w')
+                    {
+                        if (DestCol == wrapCol(SrcCol + 1))
+                        {
+                            bigFirst = false;
+                            return true;
+                        }
+                        else
+                        {
+                            if (SrcCol == 5 && DestCol == 7)
+                            {
+                                bigFirst = true;
+                                return true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (DestCol == wrapCol(SrcCol-1))
+                        {
+                            bigFirst = false;
+                            return true;
+                        }
+                        else
+                        {
+                            if (SrcCol == 10 && DestCol == 8)
+                            {
+                                bigFirst = true;
+                                return true;
+                            }
+                        }
+                    }
+                }
             }
             else
             {
-                //capture + enPassant(BY COLOR)
+                if ((DestRow == wrapRow(SrcRow - 1)) || (DestRow == wrapRow(SrcRow + 1)))
+                {
+                    if (color=='w')
+                    {
+                        if (DestCol == wrapCol(SrcCol + 1))
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        if (DestCol == wrapCol(SrcCol - 1))
+                        {
+                            return true;
+                        }
+                    }
+                }
             }
+           
+
             return false;
         }
     }
--- Common/OuterPawn.cs
+++ Common/OuterPawn.cs
@@ -23,12 +23,64 @@ namespace TorChess.Common
             Piece destPiece = board[DestRow, DestCol];
             if (destPiece == null)
             {
-                //move(BY COLOR)
+                if (DestRow == SrcRow)
+                {
+                    if (color == 'w')
+                    {
+                        if (DestCol == wrapCol(SrcCol - 1))
+                        {
+                            bigFirst = false;
+                            return true;
+                        }
+                        else
+                        {
+                            if (SrcCol == 2 && DestCol == 0)
+                            {
+                                bigFirst = true;
+                                return true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (DestCol == wrapCol(SrcCol + 1))
+                        {
+                            bigFirst = false;
+                            return true;
+                        }
+                        else
+                        {
+                            if (SrcCol == 13 && DestCol == 15)
+                            {
+                                bigFirst = true;
+                                return true;
+                            }
+                        }
+                    }
+                }
             }
             else
             {
-                //capture + enPassant(BY COLOR)
+                if ((DestRow == wrapRow(SrcRow - 1)) || (DestRow == wrapRow(SrcRow + 1)))
+                {
+                    if (color == 'w')
+                    {
+                        if (DestCol == wrapCol(SrcCol - 1))
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        if (DestCol == wrapCol(SrcCol + 1))
+                        {
+                            return true;
+                        }
+                    }
+                }
             }
+
+
             return false;
         }
     }
